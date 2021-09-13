025a32fde630ca409520a5170ecc1a45045c74ac
Jacob Oakley <jacob@oakley.codes>
2021-09-12 23:06:57 -0500

    Added home and list component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  sections: Array<any> = [
+    {
+      content: "Stack Rank is a new way to vote on the best of anything. Find a list, then drag and drop the items in the order that you think they should be ranked. Hit submit and see the results.",
+      image: "/assets/images/stack-rank-list.png",
+      title: "What is Stack Rank?",
+    },
+    {
+      content: "Getting started is easy.... Just click on the Sign In button above to be directe to the login screen, then select 'Sinup' at the bottom of the form",
+      image: "/assets/images/sign-in.png",
+      title: "How to get started?",
+    },
+    {
+      content: "Find out what your peers think by sharing your lists. You can share your list with a group or make it public for anyone to vote on. Afterwards browse lists that others have shared.",
+      image: "/assets/images/share.png",
+      title: "Share your lists",
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  list = {
+    id: "1",
+    label: "What is the best JS framework",
+    items: [
+      {name: "Angular", id: "1", rank: "1"},
+      {name: "Jquery", id: "2", rank: "2"},
+      {name: "React", id: "3", rank: "3"},
+      {name: "Vue", id: "4", rank: "4"},
+    ]
+  }
+
   constructor() { }
 
   ngOnInit(): void {
